f37e06c19ec789a2abd6c0b4b9f1def31e35d67c
♡ Sweetie drops ♡ <martinesbustosmartinbaruk@gmail.com>
2025-03-19 17:46:37 -0600

    Update programa 2.py

--- programa 2.py
+++ programa 2.py
@@ -1,4 +1,4 @@
 num1=int(input("numero de horas trabajadas"))
 num2=int(input("costo por hora"))
-multiplicacion=num1*num222
+multiplicacion=num10*num222
 print("paga correspondiente: ", num888 ,"*", num2, "es: ", multiplicacion)
